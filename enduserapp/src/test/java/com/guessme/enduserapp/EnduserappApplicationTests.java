93d69f6df29f718a42f8f2ba2444add97ded7536
ravitejav <raviteja99866@gmail.com>
2022-01-14 12:45:08 +0530

    inital commit

---
+++ enduserapp/src/test/java/com/guessme/enduserapp/EnduserappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guessme.enduserapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnduserappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
